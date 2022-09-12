7efba467caa900f322dabb5b9fbd15fc940af765
JenilsonMoraes <jenilsinhomoraes@gmail.com>
2022-09-12 11:09:56 -0300

    Testes de model de medico feitos

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -6,4 +6,16 @@ class MedicoTest < ActiveSupport::TestCase
                         especialidade: 'Reumatologista', crm: '123456'
     assert medico.save
   end
+
+  test 'cadastrando medico com crm invalido' do
+    medico = Medico.new nome: 'Mario Silva', cpf: '111.346.120-05', email: 'mario@gmail.com', especialidade: 'Neurologista', crm: '1'
+
+    assert_not medico.save
+  end
+
+  test 'cadastrando medico com especialidade invalida' do
+    medico = Medico.new nome: 'Marcos Silva', cpf: '151.446.220-05', email: 'marcos@gmail.com', especialidade: 'Ne', crm: '678970'
+
+    assert_not medico.save
+  end
 end
