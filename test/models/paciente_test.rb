29369382124bc2aee1eb2ad1d67566f07c610e9d
JenilsonMoraes <jenilsinhomoraes@gmail.com>
2022-09-12 10:58:53 -0300

    Testes de model de paciente feitos

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -4,4 +4,19 @@ class PacienteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'Cadastrar paciente com nome invalido' do
+    paciente = Paciente.new(nome: '', data_nascimento: '1997-03-12', cpf: '858.454.632-78', email: 'teste@gmail.com')
+
+    endereco = Endereco.new(logradouro: 'centro', complemento: 'casa', cep: '58521-000', bairro: 'cohab', cidade: 'Garanhuns')
+    paciente.endereco = endereco
+    assert_not paciente.save
+  end
+
+  test 'Cadastrar paciente corretamente' do
+    paciente = Paciente.new(nome: 'Jenilson', data_nascimento: '1997-04-27', cpf: '858.454.632-71', email: 'jenilson@gmail.com')
+
+    endereco = Endereco.new(logradouro: 'rua1', complemento: 'casa', cep: '58521-000', bairro: 'centro', cidade: 'Garanhuns')
+    paciente.endereco = endereco
+    assert paciente.save
+  end
 end
